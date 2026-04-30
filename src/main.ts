e5c20c67a153cd5822607cddcb4ca341c485d691
Larry <huutoan.work.47586@gmail.com>
2026-04-30 12:45:36 +0700

    Test CI CD

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
+    .setTitle('Test CI CD')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
